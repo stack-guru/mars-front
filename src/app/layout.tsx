d68eef31e3baebc3681b7c5d4f6d39b742996608
stack-guru <GoldenHand0302@gmail.com>
2025-06-25 16:36:06 -0500

    chore: remove global css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { Provider } from "@/components/ui/provider"
 
 const geistSans = Geist({
